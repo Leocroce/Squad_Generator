0a9f63979fab9e70d8fd9dba36d7fda29a8d1466
Leocroce <leo_croce@hotmail.com>
2022-01-27 07:46:54 -0300

    Adição de pipes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from './material/material.module';
+import { PipesPipe } from './pipes/pipes.pipe';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PipesPipe
+  ],
   imports: [
     CommonModule,
     MaterialModule
